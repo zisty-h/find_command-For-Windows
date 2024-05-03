a04710ce392df10cb1fef4395ccc95390de977c5
tanahiro2010 <herentongkegu087@gmail.com>
2024-05-03 12:16:41 +0900

    Update

--- f-find.py
+++ f-find.py
@@ -4,6 +4,12 @@ from time import sleep
 from datetime import datetime as time
 # get folders - get files for loop
 
+class bcolors:
+    OK = "\033[92m"  # GREEN
+    WARNING = "\033[93m"  # YELLOW
+    FAIL = "\033[91m"  # RED
+    RESET = "\033[0m"  # RESET COLOR
+
 def getFolders(root):
     objlist = os.listdir(root)
     folders = []
@@ -37,10 +43,10 @@ def main():
 
 
     for folder in folders:
-        print("Searching : " + folder)
+        print(f"{bcolors.WARNING}[WARNING {time.now()}]ANALYSIS FOLDER : {folder}{bcolors.RESET}")
         folder_result = os.listdir(folder)
         for file in folder_result:
-            print(f"[INFO {time.now()}] ANALYSIS FILE: {file}")
+            print(f"[INFO {bcolors.OK}{time.now()}{bcolors.RESET}] {bcolors.FAIL}ANALYSIS FILE{bcolors.RESET}: {file}")
             if Text in file:
                 files.append(os.path.join(root, folder, file))
                 sleep(0.1)
